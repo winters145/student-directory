48a97782267c1b7c5f8f909e0c6dd508fb009446
Jack <winterschladen.j@gmail.com>
2022-01-17 17:30:32 +0000

    Code refactored further, array and .each iteration added

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# first we print the list of students
+# All students added into an Array
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+    ]
+# Array of tsudents printed
 puts "The students of Villian Academy"
 puts "-----------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 # finally, we print the total number of students
-puts "Overall we have #{student_count} students"
+puts "Overall we have #{students.count} great students"
