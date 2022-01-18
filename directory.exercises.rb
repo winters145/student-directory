7a3fb51d5455ef83dcc8abec6d02c76b878f1f59
Jack <winterschladen.j@gmail.com>
2022-01-18 15:29:02 +0000

    Added students.csv file to save data to and added it to the .gitignore file

--- directory.exercises.rb
+++ directory.exercises.rb
@@ -66,6 +66,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. View a list of the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -81,11 +82,23 @@ def process(selection)
         input_students
       when "2"
         show_students
+      when "3"
+        save_students
       when "9"
         exit
       else
         puts "I don't know what you meant, please try again"
   end
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end  
 
 interactive_menu
