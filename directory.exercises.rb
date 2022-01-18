4f002f993baaf15ab331f7c054bb955bfe0068d2
Jack <winterschladen.j@gmail.com>
2022-01-18 15:38:02 +0000

    Added an option to load the students.csv file

--- directory.exercises.rb
+++ directory.exercises.rb
@@ -67,6 +67,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. View a list of the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -84,6 +85,8 @@ def process(selection)
         show_students
       when "3"
         save_students
+      when "4"
+        load_students
       when "9"
         exit
       else
@@ -100,5 +103,14 @@ def save_students
   end
   file.close
 end  
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end 
+  file.close
+end
 
 interactive_menu
